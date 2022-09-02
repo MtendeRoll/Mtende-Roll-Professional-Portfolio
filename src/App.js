35fa24dd2b825d7440ad54750f63d2cc991f2dc8
MtendeRoll <78053516+MtendeRoll@users.noreply.github.com>
2022-09-01 22:02:25 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 import Header from "./components/Header";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 // import Project from "./components/Project";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <div style={{ height: "100%" }}>
+      <div className="App">
+        <Header />
+      </div>
+      <Footer />
     </div>
   );
 }
