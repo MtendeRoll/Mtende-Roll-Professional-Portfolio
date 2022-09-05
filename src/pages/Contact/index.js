da931f464194a38ac16742ef1b458efc6ae76135
MtendeRoll <78053516+MtendeRoll@users.noreply.github.com>
2022-09-05 18:58:10 -0400

    updated contact form

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { validateEmail } from "../../utils/helpers";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
@@ -11,51 +10,56 @@ function Contact() {
   const { name, email, message } = formState;
 
   const [validated, setValidated] = useState(false);
+
   const handleSubmit = (event) => {
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
     }
-
+    setFormState({ ...formState, [event.target.name]: event.target.value });
     setValidated(true);
   };
 
   return (
-    <main>
-      <h2> Contact Mtende for more information! </h2>
+    <main className="mx-5 mt-5 text-center">
+      <h2 className="mb-4 col-sm-12 col-md-12 col-lg-12 contact "> Contact Mtende for more information! </h2>
       <section className="row">
         <Form noValidate validated={validated} className="col" onSubmit={handleSubmit}>
           <div className="row mb-3">
             <Form.Group className="col" controlId="formBasicName">
-              <Form.Label>Name:</Form.Label>
-              <Form.Control required type="name" placeholder="Enter name" defaultValue={name} />
+              <Form.Label className="contact">Name:</Form.Label>
+              <Form.Control className="form-input" required type="name" placeholder="Enter name" defaultValue={name} />
               <Form.Control.Feedback type="invalid">Please enter your name</Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="col" controlId="formBasicEmail">
-              <Form.Label>Email address:</Form.Label>
-              <Form.Control required type="email" placeholder="Enter email" defaultValue={email} />
-              <Form.Control.Feedback type="invalid">Please enter your email address</Form.Control.Feedback>
+              <Form.Label className="contact">Email address:</Form.Label>
+              <Form.Control className="form-input" required type="email" placeholder="Enter email" defaultValue={email} />
+              <Form.Control.Feedback type="invalid">Please enter a valid email address</Form.Control.Feedback>
               <Form.Text className="text-muted">*We'll never share your email with anyone else.</Form.Text>
             </Form.Group>
           </div>
 
           <Form.Group className="mb-3" controlId="formBasicMessage">
-            <Form.Label>Message:</Form.Label>
-            <Form.Control as="textarea" rows={3} placeholder="Enter message" defaultValue={message} />
+            <Form.Label className="contact">Message:</Form.Label>
+            <Form.Control className="form-input" as="textarea" rows={3} placeholder="Enter message" defaultValue={message} />
           </Form.Group>
 
-          <Button variant="primary" type="submit">
+          <Button className="contact-btn" variant="primary" type="submit">
             Submit
           </Button>
         </Form>
-        <div className="col">
+        <div className="col contact mt-5">
           <h5>Email</h5>
-          <p>rollmtende@gmail.com</p>
+          <a href="mailto:rollmtende@gmail.com" rel="noreferrer" target="_blank">
+            <p>rollmtende@gmail.com</p>
+          </a>
           <br />
           <h5>Phone</h5>
-          <p>336-524-1701</p>
+          <a href="tel:+13365241701">
+            <p>336-524-1701</p>
+          </a>
         </div>
       </section>
     </main>
